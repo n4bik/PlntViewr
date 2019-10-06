f160c8b55f10cea6d3b712b7f9be51b2cbdf0069
Tomasz Buga <tomaszbuga@iMac-Tomasz.local>
2019-10-06 22:46:08 +0200

    first commit

---
+++ src/app/planets/planet-card/planet-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planet-card',
+  templateUrl: './planet-card.component.html',
+  styleUrls: ['./planet-card.component.scss']
+})
+export class PlanetCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/planets/planets-container/planets-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planets-container',
+  templateUrl: './planets-container.component.html',
+  styleUrls: ['./planets-container.component.scss']
+})
+export class PlanetsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
